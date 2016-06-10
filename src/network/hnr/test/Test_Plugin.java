745e120d8b752208b81e2cfe2553a31fc6b04550
NintenGamer64-HnRNetwork <NintenGamer64-HnRNetwork@users.noreply.github.com>
2016-06-10 16:25:44 -0600

    Add files via upload

---
+++ src/network/hnr/test/Test_Plugin.java
@@ -0,0 +1,14 @@
+package network.hnr.test;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Test_Plugin extends JavaPlugin {
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
